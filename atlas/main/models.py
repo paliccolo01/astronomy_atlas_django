db62fdeaa2837b3eb81fac64e8200f6fcdff2344
Pali <gcomputermanager@hotmail.com>
2019-03-23 19:55:03 +0100

    subheading model added
    Initial model creation and adding it to the admin panel for editing

--- atlas/main/models.py
+++ atlas/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Subheading(models.Model):
+    subheading_title = models.CharField(max_length=200)
+    subheading_content = models.TextField()
+
+    def __str__(self):
+        return self.subheading_title
