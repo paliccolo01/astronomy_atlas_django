f2653b119523214ee3f688911f1dae41848550c4
Pali <gcomputermanager@hotmail.com>
2019-03-25 21:12:27 +0100

    subheading view added
    simple html view to serve the user the content of the subheadings

--- atlas/main/views.py
+++ atlas/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Subheading
 
 # Create your views here.
 
 def homepage(request):
-    return HttpResponse("Wow this is the main page")
+    return render(request=request,
+                  template_name="main/home.html",
+                  context={"subheadings": Subheading.objects.all})
